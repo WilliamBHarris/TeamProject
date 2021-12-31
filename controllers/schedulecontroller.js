2c2eddf4ef59da0effbda4089fca1311ad8d4d53
unknown <bhm727@yahoo.com>
2021-12-31 12:10:14 -0500

    Update Endpoint

--- controllers/schedulecontroller.js
+++ controllers/schedulecontroller.js
@@ -25,58 +25,57 @@ router.post("/", validateJWT, async (req, res) => {
 });
 
 router.put("/update/:id", validateJWT, async (req, res) => {
-  const { title, description, from, to } = req.body.schedule
-  const owner_id = req.params.id;
+  const { title, description, from, to } = req.body.schedule;
+  const scheduleId = req.params.id;
   const { id } = req.user;
-
   const query = {
     where: {
-      id: owner_id,
-      owner: id
-    }
-  }
-  console.log(req.body.schedule);
-  const rescheduledApt = {
-    title: title,
-    description: description,
-    from: from,
-    to: to
-  }
-
+      id: scheduleId,
+      owner_id: id,
+    },
+  };
+  const updatedSchedule = {
+    title,
+    description,
+    from,
+    to,
+    owner_id: id,
+  };
   try {
-    const update = await ScheduleModel.update(rescheduledApt, query);
-    res.status(200).json(update);
+    const updatedScheduleEntry = await ScheduleModel.update(updatedSchedule, query);
+    res.status(200).json(updatedScheduleEntry);
   } catch (err) {
+
     res.status(500).json({ error: err });
   }
-})
+});
 
-  const getUserAppointments = () => {
-    fetch('http://localhost:3000/appointments', {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      }),
-    }).then(res => res.json())
-      .then(appointments => {
-        console.log(appointments);
-        this.setState({
-          appointments: appointments
-        })
+const getUserAppointments = () => {
+  fetch('http://localhost:3000/appointments', {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    }),
+  }).then(res => res.json())
+    .then(appointments => {
+      console.log(appointments);
+      this.setState({
+        appointments: appointments
       })
-  }
-  /* Appointment Delete */
-  const cancelAppointment = (appointment) => {
-    console.log("Appointment cancelled");
-    fetch(`http://localhost:3000/appointments/cancel/{appointment.id`, {
-      method: 'DELETE',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      }),
-    }).then(() => getUserAppointments());
-  
-  }
+    })
+}
+/* Appointment Delete */
+const cancelAppointment = (appointment) => {
+  console.log("Appointment cancelled");
+  fetch(`http://localhost:3000/appointments/cancel/{appointment.id`, {
+    method: 'DELETE',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    }),
+  }).then(() => getUserAppointments());
+
+}
 
 module.exports = router;
